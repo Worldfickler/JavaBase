8094397e47bfacaf217556bb50a6f9b8a09c7cef
Fickler <2586844575@qq.com>
2024-04-16 18:03:47 +0800

    继承

--- src/main/java/org/example/day02_AbstracClass.java
+++ src/main/java/org/example/day02_AbstracClass.java
@@ -1,13 +1,129 @@
 package org.example;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
+ * 抽象类体现的就是一种模板模式的设计，抽象类作为多个子类的通用模板，子类在抽象类的基础上进行扩展，改造，同时保留抽象类的行为方式
+ *
  * @author Fickler
  * @date 2024/4/16 11:55
  */
 public class day02_AbstracClass {
 
+    public static void main(String[] args) {
 
+        Manager manager = new Manager("1", 2, 3);
+        manager.setBonus(4);
+        manager.work();
 
+//        ArrayList<? extends BaseAbstract<?>> baseAbstracts = new ArrayList<>();
+        StringBaseAbstract stringBaseAbstract = new StringBaseAbstract();
+        BaseAbstract<?> instance = BaseAbstract.getInstance(stringBaseAbstract.getKey());
+        System.out.println(instance.getContent());
+
+    }
+
+}
+
+abstract class BaseAbstract<T> {
+
+    public static HashMap<String, BaseAbstract<?>> hashMap = new HashMap<>();
+    T t;
+
+    public BaseAbstract(T t) {
+        this.t = t;
+        hashMap.put(getKey(), this);
+    }
+
+    public static BaseAbstract<?> getInstance(String key) {
+        return hashMap.get(key);
+    }
+
+    public abstract String getKey();
+
+    public abstract T getContent();
+
+}
+
+class StringBaseAbstract<T> extends BaseAbstract<T> {
+
+    private T t;
+
+    public StringBaseAbstract(T t) {
+        super(t);
+    }
+
+    @Override
+    public String getKey() {
+        return null;
+    }
+
+    @Override
+    public T getContent() {
+        return null;
+    }
+
+}
+
+abstract class Employee {
+    String name;
+    int id;
+    int salary;
+
+    public Employee(String name, int id, int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public abstract void work();
+}
+
+class Manager extends Employee {
+    int bonus;
+
+    public Manager(String name, int id, int salary) {
+        super(name, id, salary);
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName() + " " + getId() + " " + getSalary() + " " + getBonus());
+    }
 }
 
 
@@ -17,5 +133,7 @@ abstract class Animal {
     public Animal(String name) {
         this.name = name;
     }
+
+    public abstract void eat();
 
 }
